c826dd88f9a9d1290d8d04493a612aac290d102d
xuanran <xuanran.john@gmail,com>
2026-07-08 10:28:13 +0800

    fix: start bundled netease api in portable build

--- src/electron/services.js
+++ src/electron/services.js
@@ -6,8 +6,8 @@ export const NETEASE_API_PORT = 10754;
 export async function startNeteaseMusicApi() {
   ensureAnonymousToken();
   const [{ default: server }, { default: generateConfig }] = await Promise.all([
-    import('@neteasecloudmusicapienhanced/api/server'),
-    import('@neteasecloudmusicapienhanced/api/generateConfig'),
+    import('@neteasecloudmusicapienhanced/api/server.js'),
+    import('@neteasecloudmusicapienhanced/api/generateConfig.js'),
   ]);
 
   // Let user know that the service is starting
