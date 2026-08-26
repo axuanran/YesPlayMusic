480b7304810869c6b3fc7365c49e439ccfc6d0be
Xuanran <John_xuanran@outlook.com>
2026-08-26 21:18:02 +0800

    feat(mobile): load immersive lyrics player styles

--- src/main.js
+++ src/main.js
@@ -15,6 +15,7 @@ import '@/assets/css/global.scss';
 import '@/assets/css/mobile.scss';
 import '@/assets/css/mobile-adaptive.scss';
 import '@/assets/css/mobile-touch.scss';
+import '@/assets/css/mobile-lyrics-player.scss';
 import NProgress from 'nprogress';
 import '@/assets/css/nprogress.css';
 import { setupMobileShell } from '@/mobile/setupMobileShell';
